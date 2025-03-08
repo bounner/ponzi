b90d9e60a813c9355f1ab7421003bc53a5e183f8
Bounner <anykealex70@gmail.com>
2025-03-08 19:43:17 +0100

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -12,10 +12,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log(`✅ Connecté à MongoDB`))
 .catch(err => console.error(`❌ Erreur MongoDB: ${err.message}`));
 
