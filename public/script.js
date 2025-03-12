d352d42740a323b43a048c026d58e8dc844fdf7a
Bounner <anykealex70@gmail.com>
2025-03-12 13:22:30 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -30,6 +30,15 @@ async function fetchUsers() {
         console.error('Erreur lors de la récupération des utilisateurs:', err);
     }
 }
+//cpoy number
+function copyNumber(id) {
+    const text = document.getElementById(id).textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Numéro copié : " + text);
+    }).catch(err => {
+        console.error("Erreur lors de la copie :", err);
+    });
+}
 
 
 //submitdeposite
